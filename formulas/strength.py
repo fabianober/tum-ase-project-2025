2b8d457685dae28166dcc8496a79600c0b952eb3
YannisEndr <yannis@endress-hg.de>
2025-06-02 23:16:18 +0200

    Reserve Factor added

--- formulas/strength.py
+++ formulas/strength.py
@@ -1,6 +1,6 @@
 import math
 
 def strength_calc(row ,sigma_ult):
-    sigma_avg = math.sqrt(row.sigmaXX**2 + row.sigmaYY**2 - row.sigmaXX * row.sigmaYY + 3*row.sigmaXY**2)
+    sigma_avg = math.sqrt(row['sigmaXX']**2 + row['sigmaYY']**2 - row['sigmaXX'] * row['sigmaYY'] + 3*row['sigmaXY']**2)
     reserveFactor = abs(sigma_ult/(1.5*sigma_avg))
     return round(reserveFactor, 3)
