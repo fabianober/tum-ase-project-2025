5946d2d4794b38373f34c07ee5160b7f1896d8a3
Fabian Ober <fabian@familie-ober.eu>
2025-06-02 11:31:13 +0200

    add crosssectional_properties_tee_skin

--- formulas/columnbuckling.py
+++ formulas/columnbuckling.py
@@ -1,21 +1,37 @@
 import math
 
-def comb_teeStringer_skin(height_str, width_str, thickness_str, thickness_skin, stringer_pitch):
+def crosssectional_properties_tee_skin(height_str, width_str, thickness_web, thickness_flange, thickness_skin, stringer_pitch):
 
     # Individual moment of inertia calculations for the skin, flange, and web of a T-stringer
-    I_y_skin = round((stringer_pitch * thickness_skin**3) / 12, 2)
-    I_y_flange = round((width_str * thickness_str**3) / 12, 2)
-    I_y_web = round((thickness_str*(height_str-thickness_str)**3)/12, 2)
+    I_y_skin = (stringer_pitch * thickness_skin**3) / 12
+    I_y_flange = (width_str * thickness_flange**3) / 12
+    I_y_web = (thickness_web*(height_str-thickness_web)**3)/12
 
-    # combined moment of inertia (will be done)
+    # Calculate the centroid of the T-stringer
+    z_skin = -thickness_skin / 2
+    z_flange = thickness_flange/2
+    z_web = thickness_flange + (height_str - thickness_flange) / 2
 
-    return I_y_skin, I_y_flange, I_y_web
+    # Calculate the area of each component
+    A_skin = stringer_pitch * thickness_skin
+    A_flange = width_str * thickness_flange
+    A_web = thickness_web * (height_str - thickness_flange)
+    A_tot = A_skin + A_flange + A_web
 
-# Example usage
-res = comb_teeStringer_skin(height_str=45, width_str=40, thickness_str=3, thickness_skin=2, stringer_pitch=200)
-print("I_y_skin:", res[0])
-print("I_y_flange:", res[1])
-print("I_y_web:", res[2])
+    z_bar = (A_skin * z_skin + A_flange * z_flange + A_web * z_web) / A_tot
+
+
+    # combined moment of inertia
+    contrib_skin = I_y_skin + (z_skin-z_bar)**2 * A_skin
+    contrib_flange = I_y_flange + (z_flange-z_bar)**2 * A_flange
+    contrib_web = I_y_web + (z_web-z_bar)**2 * A_web
+    I_y_bar = contrib_skin + contrib_flange + contrib_web
+
+    return round(A_tot, 2), round(I_y_bar, 2)
+
+# Example usage of crosssectional_properties_tee_skin
+res = crosssectional_properties_tee_skin(height_str=45, width_str=40, thickness_web=3, thickness_flange=3, thickness_skin=2, stringer_pitch=200)
+print(f"Area: {res[0]}, Moment of Inertia: {res[1]}")
 
 def EulerBuckling(EModulus, I_y, area, length, sigma_applied, c=1):
     r = math.sqrt(I_y/area) 
