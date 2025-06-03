740aefd51cfe5d52d9b1dd426f9cfaf7cae2ad05
Fabian Ober <fabian@familie-ober.eu>
2025-06-03 12:14:45 +0200

    add task 1f file import and helper function

--- formulas/helpers.py
+++ formulas/helpers.py
@@ -1,9 +1,20 @@
 import math
+import columnbuckling as colbuckl
 
 def lmd(I_y, area, length, c=1):
     r = math.sqrt(I_y/area) 
     lmd = (c*length)/r
     return round(lmd, 2)
+
+def crosssectional_properties_tee_skin_row(row):
+    return colbuckl.crosssectional_properties_tee_skin(
+        height_str=row['height_str'],
+        width_str=row['width_str'],
+        thickness_web=row['thickness_web'],
+        thickness_flange=row['thickness_flange'],
+        thickness_skin=row['thickness_skin'],
+        stringer_pitch=row['stringer_pitch']
+    )
 
 #Running test on all functions 
 if __name__ == '__main__':
